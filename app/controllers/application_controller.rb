f6455d7e40f85b10b08d9f160a267c48d512c0ef
Adam Wheatley <adamwheatley7@gmail.com>
2024-08-16 14:20:53 +0000

    User Profile Added profile page Edit profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :phone_number, :full_name, :password_confirmation])
         devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:full_name, :username, :email, :password, :password_confirmation, :current_password])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:full_name, :phone_number, :username, :profile_pic, :password, :password_confirmation, :current_password])
     end
 end
